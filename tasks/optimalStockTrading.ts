1eacfe46861c0d93dcefef4506006b42e9d6379d
Alfredo Sumaran <a.sumaran@gmail.com>
2025-12-14 10:41:02 -0500

    optimalStockTrading

--- tasks/optimalStockTrading.ts
+++ tasks/optimalStockTrading.ts
@@ -6,11 +6,23 @@ export default function optimalStockTrading(prices: number[]): number {
     // el tema era comparar contra el precio actual
     maxProfit = Math.max(maxProfit, prices[i] - lowestPrice);
   }
-
   return maxProfit;
+
+  // Approach 2, fuerza bruta
+  // let maxProfit = 0;
+  // for (let i = 0; i < prices.length; i++) {
+  //   for (let j = i + 1; j < prices.length; j++) {
+  //     console.log(prices[i], prices[j]);
+  //     const profit = prices[j] - prices[i];
+  //     if (profit > maxProfit) {
+  //       maxProfit = profit;
+  //     }
+  //   }
+  // }
+  // return maxProfit;
 }
 
 // const test = [4, 3, 2, 1];
 // const test = [6, 8, 1, 2, 30, 19]; // 29
-const test = [3, 5, 4, 10, 4, 0]; // 7
+const test = [1, 2, 3, 4]; // 7
 console.log(optimalStockTrading(test));
