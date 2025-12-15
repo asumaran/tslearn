89f50d875c360c25299665f2f66488261ac38701
Alfredo Sumaran <a.sumaran@gmail.com>
2025-12-15 10:31:26 -0500

    pairSum

--- tasks/pairSum.ts
+++ tasks/pairSum.ts
@@ -1,16 +1,26 @@
 export default function pairSum(numbers: number[], target: number): number[] {
-  const hash: Record<number, number> = {};
-  numbers.forEach((v, k) => (hash[v] = k));
+  // const hash: Record<number, number> = {};
+  // numbers.forEach((v, k) => (hash[v] = k));
+  // for (let i = 0; i < numbers.length; i++) {
+  //   const complement = target - numbers[i];
+  //   // debemos usar in en vez de hash[complement]
+  //   // porque de ser cero el valor no pasa la evaluacion al ser evaluado como falsy
+  //   if (complement in hash && hash[complement] !== i) {
+  //     return [i, hash[complement]];
+  //   }
+  // }
+  // return [];
 
+  // better approach
+  let hash: Record<number, number> = {};
   for (let i = 0; i < numbers.length; i++) {
     const complement = target - numbers[i];
-    // debemos usar in en vez de hash[complement]
-    // porque de ser cero el valor no pasa la evaluacion al ser evaluado como falsy
     if (complement in hash && hash[complement] !== i) {
-      return [i, hash[complement]];
+      return [hash[complement], i];
     }
-  }
 
+    hash[numbers[i]] = i;
+  }
   return [];
 }
 
@@ -19,7 +29,7 @@ export default function pairSum(numbers: number[], target: number): number[] {
 
 // este caso retorna [1, 1] sin el check adicional
 // pero con el check retorna [2, 3] correctamente.
-const numbers = [1, -174, -164, -184];
-const target = -348;
+const numbers = [0, 7, 1, 9];
+const target = 7;
 
 console.log(pairSum(numbers, target));
